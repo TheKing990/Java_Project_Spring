1f1956e7179c60064ffc3123bf66440cbb82e835
Jhovany Gonzalez <jhovany990@gmail.com>
2016-03-16 22:31:56 -0500

    fix button need to fix title

--- src/main.java
+++ src/main.java
@@ -3,11 +3,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
 public class main extends Application {
 
     Stage window;
-    Button button;
+    Button button1,button2;
+
 
     public static void main(String[] args)
 
@@ -17,14 +19,41 @@ public class main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws Exception
+    {
         window = primaryStage;
-        window.setTitle("Getting Started");
-        button = new Button("Click me");
+        window.setTitle("Bank of the Internet");
+
+
+
+        button2 = new Button("log in");
+        button1 = new Button("sign up");
+
+        button1.setStyle("-fx-font: 22 arial; -fx-base: #b6e7c9;");
+        button1.setTranslateX(120);
+        button1.setTranslateY(0);
+
+        button2.setStyle("-fx-font: 22 arial; -fx-base: #b6e7c9;");
+        button2.setTranslateX(-120);
+        button2.setTranslateY(0);
+
+        Text t = new Text();
+        t.setText("Bank of The Internet");
+        t.setFont(Font.font ("Verdana", 40));
+        t.setTranslateX(0);
+        t.setTranslateY(-120);
+
 
         StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-        Scene scene = new Scene(layout, 300, 250);
+        layout.getChildren().add(button2);
+        layout.getChildren().add(button1);
+        layout.getChildren().add(t);
+        Scene scene = new Scene(layout, 800, 700);
+
+
+
+
+
 
         window.setScene(scene);
         window.show();
